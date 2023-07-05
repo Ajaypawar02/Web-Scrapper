6a1cb138afb0d2cad2f3590665baf845f5a1aeb6
Ajay Pawar <65480235+Ajaypawar02@users.noreply.github.com>
2023-07-05 13:39:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import sys
 sys.path.append("/home/ajay/Listed_2/src")
 from scrapper import Scrapper_Web
 from bs4 import BeautifulSoup
+from youtube_scrapper import scapping_youtube
 
 
 
 if __name__ == "__main__":
-    scrapper = Scrapper_Web(15, "site:youtube.com openinapp.co")
-    links = scrapper.get_url()
-    print(links)
+    scrapper = scapping_youtube()
+    df = pd.DataFrame(scrapper, columns = ["Links"])
+    df.iloc[:10000]
+    df.to_csv("youtube_liinks_10000.csv", index = False)
+    print(df)
